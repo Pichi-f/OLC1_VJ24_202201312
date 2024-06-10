6c6ba7fb8a244cb904e28814cc55efc2ffb27386
Luis Pichiyá <3502658870401@ingenieria.usac.edu.gt>
2024-06-10 02:22:08 -0600

    Pruebas
    Probando prints de cadenas

--- Interprete/src/interprete/Interprete.java
+++ Interprete/src/interprete/Interprete.java
@@ -4,6 +4,15 @@
  */
 package interprete;
 
+import abstracto.Instruccion;
+import analisis.parser;
+import analisis.scanner;
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.LinkedList;
+import simbolo.Arbol;
+import simbolo.tablaSimbolos;
+
 /**
  *
  * @author pichi
@@ -15,6 +24,24 @@ public class Interprete {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            String texto = "imprimir(\"Mi cadena 33\");"
+                    + "imprimir(2); imprimir(3.33);";
+            scanner s = new scanner(new BufferedReader(new StringReader(texto)));
+            parser p = new parser(s);
+            var resultado = p.parse();
+            var ast = new Arbol((LinkedList<Instruccion>) resultado.value);
+            var tabla = new tablaSimbolos();
+            tabla.setNombre("GLOBAL");
+            ast.setConsola("");
+            for(var a : ast.getInstrucciones()){
+                var res = a.interpretar(ast, tabla);
+            }
+            System.out.println(ast.getConsola());
+        } catch (Exception ex) {
+            System.out.println("Algo salio mal");
+            System.out.println(ex);
+        }
     }
     
 }
