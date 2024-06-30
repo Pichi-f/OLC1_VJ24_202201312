bab17a53567112eb6deda230c22cce377812a1c7
Luis Pichiyá <3502658870401@ingenieria.usac.edu.gt>
2024-06-29 23:01:05 -0600

    Llamadas
    Finalizando con llamadas

--- Interprete/src/instrucciones/Declaracion.java
+++ Interprete/src/instrucciones/Declaracion.java
@@ -16,10 +16,11 @@ import simbolo.tipoDato;
  *
  * @author pichi
  */
-public class Declaracion extends Instruccion{
+public class Declaracion extends Instruccion {
+
+    public String mutabilidad;
     public String identificador;
     public Instruccion valor;
-    public String mutabilidad;
 
     //aqui se veran los valores por defecto, tomarlo en cuenta
     public Declaracion(String mutabilidad, String identificador, Instruccion valor, Tipo tipo, int linea, int col) {
@@ -29,36 +30,43 @@ public class Declaracion extends Instruccion{
         this.valor = valor;
     }
 
+    public Declaracion(String mutabilidad, String identificador, Tipo tipo, int linea, int col) {
+        super(tipo, linea, col);
+        this.mutabilidad = mutabilidad;
+        this.identificador = identificador;
+        this.valor = null;
+    }
+
     @Override
     public Object interpretar(Arbol arbol, tablaSimbolos tabla) {
         // Interpretar la expresión
         var valorInterpretado = (this.valor == null) ? this.valoresDefault() : this.valor.interpretar(arbol, tabla);
         // Se valida si es error
-        if (valorInterpretado instanceof Errores){
+        if (valorInterpretado instanceof Errores) {
             return valorInterpretado;
         }
         // Se valida el tipo
-        if (this.valor != null){
-            if (this.valor.tipo.getTipo() != this.tipo.getTipo()){
+        if (this.valor != null) {
+            if (this.valor.tipo.getTipo() != this.tipo.getTipo()) {
                 return new Errores("SEMANTICO", "Tipos Erroneos", this.linea, this.col);
             }
         }
-        
+
         //Se crea el simbolo
         Simbolo s = new Simbolo(this.tipo, this.identificador, valorInterpretado);
-        if (mutabilidad.toLowerCase().equals("const")){
+        if (mutabilidad.toLowerCase().equals("const")) {
             s.setMutabilidad(true);
             System.out.println("Valor constante");
         }
-        
+
         boolean creacion = tabla.setVariable(s);
-        if (!creacion){
+        if (!creacion) {
             return new Errores("SEMANTICO", "Variable ya existente", this.linea, this.col);
         }
-        
+
         return null;
     }
-    
+
     public Object valoresDefault() {
         return switch (this.tipo.getTipo()) {
             case tipoDato.BOOLEANO ->
